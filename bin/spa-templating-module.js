e63381876ed5ceef2b8c5889428265ffe1422e1a
Mason L’Amy <M.lamy@marketinvoice.com>
2017-03-21 12:40:05 +0000

    Add basic presentational template functionality

--- bin/spa-templating-module.js
+++ bin/spa-templating-module.js
@@ -1,13 +1,37 @@
 #!/usr/bin/env node
 
+const fs = require('fs');
 const path = require('path');
 const libs = require(path.join('../lib'));
 const argv = require('yargs').argv;
-
-console.log('executing in directory: '.concat(process.cwd()));
+const colors = require('colors');
 
 if (argv.p) {
-    console.log(argv.p);
+    try {
+        const template = libs.templatePresentational(argv.p);
+        const filename = argv.p.concat('.js');
+        console.log('Creating file '.concat(filename).concat('...'));
+
+        fs.open(filename, 'r', function(err, fd) {
+            if (err) {
+                fs.writeFile(filename, template, function(err){
+                    if (err) {
+                        console.error(err.toString().red);
+                    }
+                    else {
+                        console.log('Successful'.green);
+                    }
+                });
+            }
+            else {
+                console.error('File already exists!'.red);
+            }
+        });
+    }
+    catch (err) {
+        console.log(err.toString().red);
+        return;
+    }
 }
 else if (argv.c) {
     console.log(argv.c);
