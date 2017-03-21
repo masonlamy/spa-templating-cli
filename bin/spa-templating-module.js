760e66379ed993b50ac137e13c1ef96b6b2a160c
Mason L’Amy <M.lamy@marketinvoice.com>
2017-03-21 15:59:38 +0000

    Implement presentational/container template and unit test scaffolding

--- bin/spa-templating-module.js
+++ bin/spa-templating-module.js
@@ -6,27 +6,18 @@ const libs = require(path.join('../lib'));
 const argv = require('yargs').argv;
 const colors = require('colors');
 
+var filename;
+var template;
+
+var templateTests;
+var filenameTests;
+
 if (argv.p) {
     try {
-        const template = libs.templatePresentational(argv.p);
-        const filename = argv.p.concat('.js');
-        console.log('Creating file '.concat(filename).concat('...'));
-
-        fs.open(filename, 'r', function(err, fd) {
-            if (err) {
-                fs.writeFile(filename, template, function(err){
-                    if (err) {
-                        console.error(err.toString().red);
-                    }
-                    else {
-                        console.log('Successful'.green);
-                    }
-                });
-            }
-            else {
-                console.error('File already exists!'.red);
-            }
-        });
+        template = libs.templatePresentational(argv.p);
+        templateTests = libs.templatePresentationalTests(argv.p);
+        filename = argv.p.concat('.js');
+        filenameTests = argv.p.concat('.test.js');
     }
     catch (err) {
         console.log(err.toString().red);
@@ -34,5 +25,53 @@ if (argv.p) {
     }
 }
 else if (argv.c) {
-    console.log(argv.c);
+    try {
+        template = libs.templateContainer(argv.c);
+        templateTests = libs.templateContainerTests(argv.c);
+        filename = argv.c.concat('Container').concat('.js');
+        filenameTests = argv.c.concat('Container.test.js');
+    }
+    catch (err) {
+        console.log(err.toString().red);
+        return;
+    }
 }
+else {
+    return;
+}
+
+console.log('Creating file '.concat(filename).concat('...'));
+
+fs.open(filename, 'r', function(err, fd) {
+    if (err) {
+        fs.writeFile(filename, template, function(err){
+            if (err) {
+                console.error(err.toString().red);
+            }
+            else {
+                console.log('Successful'.green);
+            }
+        });
+    }
+    else {
+        console.error('File already exists!'.red);
+    }
+});
+
+console.log('Creating file '.concat(filenameTests).concat('...'));
+
+fs.open(filename, 'r', function(err, fd) {
+    if (err) {
+        fs.writeFile(filenameTests, templateTests, function(err){
+            if (err) {
+                console.error(err.toString().red);
+            }
+            else {
+                console.log('Successful'.green);
+            }
+        });
+    }
+    else {
+        console.error('File already exists!'.red);
+    }
+});
